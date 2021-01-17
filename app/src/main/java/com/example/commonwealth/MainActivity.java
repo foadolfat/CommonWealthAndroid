bb61c0c21b6cf3ba68f7d28faaf3429b8d9c6e79
foadolfat <olfatfoad@gmail.com>
2021-01-17 10:52:26 -0800

    Basic read to and write from firebase implemented

--- app/src/main/java/com/example/commonwealth/MainActivity.java
+++ app/src/main/java/com/example/commonwealth/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.commonwealth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,22 +8,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private Button button;
     private FirebaseAuth mAuth;
+
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+
+
+
+        db.collection("Projects").document("First Test Project").get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                     @Override
+                     public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists()){
+                            String project1 = documentSnapshot.getString("project_name");
+                            textView.setText(project1);
+                        }else{
+                            Toast.makeText(MainActivity.this, "Document does not exist", Toast.LENGTH_SHORT).show();
+                        }
+                     }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Add_project();
+        @Override
+        public void onClick(View v) {
+              Add_project();
             }
         });
     }
@@ -31,4 +74,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateNewProject.class);
         startActivity(intent);
     }
+
 }

037a3dcabacc39a94aaadb0822cd6a787ddc2290
foadolfat <olfatfoad@gmail.com>
2021-01-18 21:16:53 -0800

    Added firestore queries to display top 5 projects with the most helpers.

--- app/src/main/java/com/example/commonwealth/MainActivity.java
+++ app/src/main/java/com/example/commonwealth/MainActivity.java
@@ -16,6 +16,9 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -23,11 +26,15 @@ public class MainActivity extends AppCompatActivity {
     private Button search;
     private FirebaseAuth mAuth;
 
+    TextView[] textView= new TextView[5];
+    /*
     TextView textView;
     TextView textView2;
     TextView textView3;
     TextView textView4;
     TextView textView5;
+     */
+
 
     EditText search_docs;
 
@@ -41,14 +48,39 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         search = findViewById(R.id.button3);
-        textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
-        textView3 = findViewById(R.id.textView3);
-        textView4 = findViewById(R.id.textView4);
-        textView5 = findViewById(R.id.textView5);
+        textView[0] = findViewById(R.id.textView);
+        textView[1] = findViewById(R.id.textView2);
+        textView[2] = findViewById(R.id.textView3);
+        textView[3] = findViewById(R.id.textView4);
+        textView[4] = findViewById(R.id.textView5);
         search_docs = findViewById(R.id.search_docs);
 
+        db.collection("Projects").whereGreaterThan("numOfHelpers", 1)
+                .orderBy("numOfHelpers", Query.Direction.DESCENDING)
+                .limit(5)
+                //.document(search_docs.getText()
+                //.toString())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int i=0;
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                            CWProject recieving = new CWProject();
+                            recieving = documentSnapshot.toObject(CWProject.class);
+                            textView[i].setText(recieving.getProjectName());
+                            i++;
+                        }
 
+                        //textView4.setText(recieving.getNumOfHelpers());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
 
 
@@ -74,7 +106,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void search_docs(){
-        db.collection("Projects").document(search_docs.getText().toString()).get()
+        db.collection("Projects").whereEqualTo("projectName", search_docs.getText().toString())
+                //.document(search_docs.getText()
+                //.toString())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                            CWProject recieving = new CWProject();
+                            recieving = documentSnapshot.toObject(CWProject.class);
+                            textView[0].setText(recieving.getProjectName());
+                            textView[1].setText(recieving.getName());
+                            textView[2].setText(recieving.getLocation());
+                            textView[3].setText("" + recieving.getNumOfHelpers());
+                            textView[4].setText("");
+                        }
+
+                        //textView4.setText(recieving.getNumOfHelpers());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+}
+
+
+/*
+db.collection("Projects")
+                .document(search_docs.getText().toString())
+                .get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
@@ -97,6 +163,4 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
                     }
                 });
-    }
-
-}
+ */
